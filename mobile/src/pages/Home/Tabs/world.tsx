78062208245abf3d15588ddcbacc4c6afbdfe1ea
lordradamanthys <mateuslima565@gmail.com>
2020-08-15 02:57:44 -0300

    made tab screens from home page and done hes styles

---
+++ mobile/src/pages/Home/Tabs/world.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import { View, Picker, Text, StyleSheet, Image } from 'react-native'
+import Constants from 'expo-constants'
+import { Feather } from '@expo/vector-icons'
+
+
+const TabWorld = () => {
+    const [dataPickerCountry, setDataPickerCountry] = useState(['teste', 'teste1'])
+    const [selectedPickerCity, setSelectedPickerCity] = useState('')
+
+    function getValuePickerCity(value: string) {
+        setSelectedPickerCity(value)
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.pickerSelectCityContainer}>
+                <View style={styles.input}>
+                    <Feather style={{ marginEnd: 10 }} name="map" size={25} color="#4799F7" />
+                    <Picker
+
+                        selectedValue={selectedPickerCity}
+                        style={{ width: '90%' }}
+                        onValueChange={(itemValue, itemIndex) => getValuePickerCity(itemValue)}
+                    >
+                        <Picker.Item label="Select another city" value="" color='rgba(88, 88, 88, 0.4)' />
+                        <Picker.Item label="Java" value="java" color='black' />
+                        <Picker.Item label="JavaScript" value="js" color='black' />
+                    </Picker>
+                </View>
+            </View>
+
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+
+   
+    container: {
+        flex: 1,
+        backgroundColor: '#FAFAFA'
+    },
+
+    pickerSelectCityContainer: {
+        justifyContent: 'space-between',
+        padding: 10,
+        flexDirection: 'row',
+      //  backgroundColor: '#f3f'
+    },
+
+    calendarInfoHeader: {
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+    },
+
+    titleHeader: {
+        fontSize: 24,
+        color: '#585858',
+        fontFamily: 'Inter_500Medium'
+    },
+    input: {
+        flexDirection: 'row',
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        backgroundColor: 'rgba(124, 119, 119, 0.15)',
+        borderRadius: 5,
+        marginBottom: 30,
+        // width: '80%',
+        alignSelf: 'center',
+        paddingHorizontal: 24,
+        fontSize: 16,
+    },
+})
+
+export default TabWorld
--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,47 +1,65 @@
 import React from 'react'
 import { View, Picker, Text, StyleSheet, Image } from 'react-native'
 import Constants from 'expo-constants'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import TabPersonal from './Tabs/personal';
+import TabWorld from './Tabs/world';
 
 const Home = () => {
+    const Tab = createMaterialTopTabNavigator();
 
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image style={{ width: 50, height: 50 }} source={require('../../assets/logo.png')} />
-                <Text>Hi Mateus </Text>
+                <Text style={[styles.textHeader, { alignSelf: 'center', marginLeft: 20 }]}>Hi Mateus </Text>
                 <View style={styles.calendarInfoHeader}>
-                    <Text>Sao Paulo, Brasil</Text>
-                    <Text style={{alignSelf:'flex-end'}}>21/08/2020</Text>
+                    <Text style={styles.textHeader}>Sao Paulo, Brasil</Text>
+                    <Text style={[styles.textHeader, { alignSelf: 'flex-end' }]}>21/08/2020</Text>
                 </View>
             </View>
+
+            <Tab.Navigator tabBarOptions={{
+                tabStyle: { borderRadius: 5 },
+                labelStyle: { fontFamily: 'Inter_400Regular', fontSize: 19 },
+                activeTintColor: '#FFF',
+                inactiveTintColor: '#585858',
+                pressColor: '#4799F7',
+                style: { backgroundColor: '#F0F0F0', elevation: 9 },
+                indicatorStyle: { height: '100%',  backgroundColor: '#4799F7', borderRadius: 5 },
+            }}>
+                <Tab.Screen name="Brasil" component={TabPersonal} />
+                <Tab.Screen name="World" component={TabWorld} />
+            </Tab.Navigator>
         </View>
     )
 }
 
 
 const styles = StyleSheet.create({
+
     container: {
         flex: 1,
         paddingTop: Constants.statusBarHeight + 15,
-        backgroundColor: '#FAFAFA'
+        backgroundColor: '#F0F0F0'
     },
 
     header: {
         justifyContent: 'space-between',
         padding: 30,
         flexDirection: 'row',
-        backgroundColor: '#f3f'
+        backgroundColor: '#F0F0F0'
     },
 
-    calendarInfoHeader:{
-        flexDirection:'column',
-        justifyContent:'space-between'
+    calendarInfoHeader: {
+        flexDirection: 'column',
+        justifyContent: 'space-between'
     },
 
-    titleHeader: {
-        fontSize: 24,
+    textHeader: {
+        fontSize: 14,
         color: '#585858',
-        fontFamily: 'Inter_500Medium'
+        fontFamily: 'Inter_400Regular'
     },
 })
 
