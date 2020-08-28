8b99df1b73468e48387835aa07a4539f52eccea1
Mateus Lima <mateuslima@pop-os.localdomain>
2020-08-28 02:41:09 -0300

    style info number of deaths and cases

--- mobile/src/pages/Home/Tabs/personal.tsx
+++ mobile/src/pages/Home/Tabs/personal.tsx
@@ -13,17 +13,23 @@ import {
     StackedBarChart
 } from "react-native-chart-kit";
 import api from '../../../services/api';
+import { ScrollView } from 'react-native-gesture-handler'
 
 
 
 const TabPersonal = () => {
     const route = useRoute()
-    const [dataPickerCountry, setDataPickerCountry] = useState(['teste', 'teste1'])
     const [selectedPickerCity, setSelectedPickerCity] = useState('')
-
     const [statesDeaths, setStateDeaths] = useState(0)
     const [statesCases, setStateCases] = useState(0)
+
     const [dataStates, setDataStates] = useState()
+    const [dataCountry, setDataCountry] = useState()
+    const [dataCountryDeaths, setDataCountryDeaths] = useState()
+    const [dataCountryCases, setDataCountryCases] = useState()
+    const [dataCountryConfirmed, setDataCountryConfirmed] = useState()
+    const [dataCountryRecovered, setDataCountryRecovered] = useState()
+
     function getValuePickerCity(value: string) {
         setSelectedPickerCity(value)
     }
@@ -31,9 +37,19 @@ const TabPersonal = () => {
     async function getAllStatusOfState(state: string = 'sp') {
         api.get(`v1/brazil/uf/${state}`).then(response => {
 
-            setStateDeaths(response.data.deaths)
-            setStateCases(response.data.cases)
-            getStatusOfStateToday(response.data.uf)
+            const formatDate = {
+                labels: ["Mortes", "Casos", "suspeitos"],
+                datasets: [
+                    {
+                        data: [response.data.deaths, response.data.cases, response.data.suspects]
+                    }
+                ]
+            };
+            // setStateDeaths(response.data.deaths)
+            // setStateCases(response.data.cases)
+            setDataStates(formatDate)
+
+
         }).catch(erro => {
             console.log(erro)
         })
@@ -42,34 +58,31 @@ const TabPersonal = () => {
 
     function getFormatData() {
         const date = new Date()
-        if(date.getMonth()>9){
+        if (date.getMonth() > 9) {
             return `${date.getFullYear()}${date.getMonth()}${date.getDate()}`
-        }else{
+        } else {
             return `${date.getFullYear()}0${date.getMonth()}${date.getDate()}`
         }
     }
 
 
-    async function getStatusOfStateToday(uf: string) {
+    async function getStatusOfCountry(uf: string = 'brazil') {
 
-        api.get(`v1/brazil/${getFormatData()}`).then(response => {
-
-            const data = response.data.data.filter((data) => {
-                if (data.uf === uf) {
-                    return data
-                }
-            })
-
+        api.get(`v1/brazil`).then(response => {
             const formatDate = {
-                labels: ["Mortes", "Casos", "Suspeitos"],
+                labels: ["Mortes", "Casos", "Confirmados"],
                 datasets: [
                     {
-                        data: [data[0].deaths, (data[0].cases), data[0].suspects]
+                        data: [response.data.data.deaths, response.data.data.cases, response.data.data.confirmed]
                     }
                 ]
             };
 
-            setDataStates(formatDate)
+            setDataCountryCases(response.data.data.cases)
+            setDataCountryDeaths(response.data.data.deaths)
+            setDataCountryConfirmed(response.data.data.confirmed)
+            setDataCountryRecovered(response.data.data.recovered)
+            //setDataCountry(formatDate)
 
         }).catch(erro => {
             console.log(erro)
@@ -78,10 +91,10 @@ const TabPersonal = () => {
 
 
     const dataInit = {
-        labels: ["January", "February", "March", "April", "May", "June"],
+        labels: ["", "", "", ""],
         datasets: [
             {
-                data: [20, 45, 28, 80, 99, 43]
+                data: [0, 0, 0, 0]
 
             },
 
@@ -101,8 +114,11 @@ const TabPersonal = () => {
         useShadowColorFromDataset: false // optional
     };
 
-
+    function numberWithCommas(x: number) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
     useEffect(() => {
+        getStatusOfCountry()
         const value = !selectedPickerCity ? route.params : selectedPickerCity
         getAllStatusOfState(value)
         // getStatusOfStateToday()
@@ -110,6 +126,23 @@ const TabPersonal = () => {
 
     return (
         <View style={styles.container}>
+
+            <View style={styles.containerStatus}>
+                <Text style={[styles.title, { marginLeft: 100 }]}>Total</Text>
+                <View style={styles.containerDataStatus}>
+                    <View style={[styles.status, { backgroundColor: '#CE2727' }]}>
+                        <Text style={{ color: '#fff', fontFamily: 'Inter_500Medium' }}>{`${numberWithCommas((Number(dataCountryDeaths)))}`}</Text>
+                        <Text style={{ color: '#fff', fontFamily: 'Inter_400Regular' }}>Mortes</Text>
+                    </View>
+
+                    <View style={[styles.status, { backgroundColor: '#FFD600' }]}>
+                        <Text style={{ color: 'black', fontFamily: 'Inter_500Medium' }}>{`${numberWithCommas((Number(dataCountryConfirmed)))}`}</Text>
+                        <Text style={{ color: 'black', fontFamily: 'Inter_400Regular' }}>Casos</Text>
+                    </View>
+
+                </View>
+            </View>
+
             <View style={styles.pickerSelectCityContainer}>
                 <View style={styles.selectInput}>
                     <Feather style={{ marginEnd: 10 }} name="map" size={25} color="#4799F7" />
@@ -130,21 +163,6 @@ const TabPersonal = () => {
 
             </View>
 
-            <View style={styles.containerStatus}>
-                <Text style={styles.title}>Total</Text>
-                <View style={styles.containerDataStatus}>
-                    <View style={[styles.status, { backgroundColor: '#CE2727' }]}>
-                        <Text style={{ color: '#fff' }}>{`${statesDeaths} mortes`}</Text>
-                    </View>
-
-                    <View style={[styles.status, { backgroundColor: '#FFD600' }]}>
-                        <Text style={{ color: 'black' }}>{`${statesCases} casos`}</Text>
-                    </View>
-
-
-                </View>
-
-            </View>
             <View style={{ justifyContent: 'center', alignItems: 'center', marginHorizontal: 60 }}>
                 <Text style={[styles.title, { marginVertical: 15 }]}>Hoje</Text>
                 <BarChart
@@ -155,6 +173,7 @@ const TabPersonal = () => {
                     showValuesOnTopOfBars={true}
                     style={{ borderRadius: 10 }}
                     fromZero={true}
+                    segments={1}
                     data={!dataStates ? dataInit : dataStates}
                     width={Dimensions.get("window").width - 20}
                     height={280}
@@ -203,8 +222,8 @@ const styles = StyleSheet.create({
     },
 
     containerStatus: {
+        marginTop:10,
         flexDirection: 'row',
-        marginLeft: 10,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F0F0F0'
@@ -214,23 +233,26 @@ const styles = StyleSheet.create({
     containerDataStatus: {
         flexDirection: 'row',
         padding: 10,
+        paddingRight: 90,
         justifyContent: 'center',
         // backgroundColor: 'black'
 
     },
 
     status: {
-        paddingVertical: 20,
-        paddingHorizontal: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
         marginHorizontal: 10,
         borderRadius: 5
         // backgroundColor: 'red'
     },
 
     title: {
+
         fontFamily: 'Inter_500Medium',
         fontSize: 22,
-        color:  false ? '#fff' : '#585858',
+        color: false ? '#fff' : '#585858',
     },
 })
 
