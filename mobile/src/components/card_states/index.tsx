509fc5f588421ab733427f59aa20fe279f3604c8
Mateus Lima <mateuslima@pop-os.localdomain>
2020-08-30 00:02:14 -0300

    color of card

--- mobile/src/components/card_states/index.tsx
+++ mobile/src/components/card_states/index.tsx
@@ -7,7 +7,7 @@ const CardState = () => {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Sao Paulo</Text>
+            <Text style={[styles.title, { color: 'black' }]}>Sao Paulo</Text>
             <View style={styles.infoContainer}>
 
                 <View style={styles.info}>
@@ -15,11 +15,11 @@ const CardState = () => {
                     <Text style={styles.title}>3342234234</Text>
                 </View>
 
-                <View style={styles.info}>
-                    <Text style={styles.title}>Casos</Text>
-                    <Text style={styles.title}>234342</Text>
+                <View style={[styles.info, { backgroundColor: '#FFD600' }]}>
+                    <Text style={[styles.title, { color: 'black' }]}>Casos</Text>
+                    <Text style={[styles.title, { color: 'black' }]}>234342</Text>
                 </View>
-                
+
             </View>
         </View>
     )
@@ -29,10 +29,12 @@ export default CardState
 
 const styles = StyleSheet.create({
     container: {
+        margin: 20,
+        padding: 15,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'black',
-        borderRadius: 10
+        backgroundColor: 'rgba(160, 160, 160, 0.15)',
+        borderRadius: 5
     },
 
     infoContainer: {
@@ -43,13 +45,13 @@ const styles = StyleSheet.create({
         color: 'white'
     },
 
-    info:{
-        margin:20,
-        backgroundColor:'red',
+    info: {
+        margin: 20,
+        backgroundColor: 'red',
         justifyContent: 'center',
         alignItems: 'center',
-        flexDirection:'column',
-        padding: 20,
+        flexDirection: 'column',
+        padding: 30,
         marginHorizontal: 10,
         borderRadius: 5
     }
