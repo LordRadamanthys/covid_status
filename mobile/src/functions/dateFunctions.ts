14a0e4d20aee9ba3a7b23a28208e9d8cc30e0256
Mateus Lima <mateuslima@pop-os.localdomain>
2020-09-09 23:11:52 -0300

    refactore get data function

--- mobile/src/functions/dateFunctions.ts
+++ mobile/src/functions/dateFunctions.ts
@@ -3,9 +3,9 @@ export function getDate() {
     const date = new Date()
     let dateNow = ''
     if (date.getDay() < 10 && date.getMonth() < 10) {
-       return dateNow = `0${date.getDay()}/0${date.getMonth()}/${date.getFullYear()}`
+       return dateNow = `0${date.getDay()}/0${date.getMonth()+1}/${date.getFullYear()}`
     } else {
-        return dateNow = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`
+        return dateNow = `${date.getDay()}/${date.getMonth()+1}/${date.getFullYear()}`
 
     }
 }
